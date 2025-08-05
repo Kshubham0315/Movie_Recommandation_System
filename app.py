116c5af9e12f56ecb6ce4a90d0032644ec29552a
Shubham Kumar <kshubham0315@gmail.com>
2025-08-05 17:57:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 import streamlit as st
 import pickle
-
-# Load the movie DataFrame
 with open('recommandation2.sav', 'rb') as f:
     new_df = pickle.load(f)
-
-# Load the similarity matrix
 with open('recommandation.sav', 'rb') as f:
     similarity = pickle.load(f)
-
-# Recommend function
 def recommend(movie):
     movie_index = new_df[new_df['title'] == movie].index[0]
     distances = similarity[movie_index]
     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     return [new_df.iloc[i[0]].title for i in movie_list]
-
-# Streamlit UI
 st.set_page_config(page_title="Movie Recommender", layout="centered")
-st.title("🎬 Movie Recommendation System")
-
+st.title("Movie Recommendation System")
 selected_movie = st.selectbox("Choose a movie", new_df['title'].values)
-
 if st.button("Get Recommendations"):
     recommendations = recommend(selected_movie)
     st.subheader("Top 5 Recommended Movies:")
